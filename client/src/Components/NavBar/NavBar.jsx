3c1358ae3aedb087e7fc41fdde61e4fd31292fd7
maguilococo <56405247+maguilococo@users.noreply.github.com>
2021-05-07 11:38:22 -0300

    Fixes #19

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+/* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faAddressCard } from '@fortawesome/free-solid-svg-icons'; */
+import logo from '../../images/circulo.png';
 import style from './NavBar.module.css';
 
+// const classNames = require('classnames');
+
 function NavBar() {
-  // eslint-disable-next-line no-unused-vars
-  const hla = 'hola';
   return (
-    <nav className={style.ctn}>
-      <div className={style.divH3}>
-        <NavLink exact to="/" activeStyle={{ borderBottom: '1px solid white' }}>
-          <h3 className={style.h3}>
-            <FontAwesomeIcon icon={faHome} />
-            { ' Home' }
-          </h3>
-        </NavLink>
-        <NavLink to="/about" activeStyle={{ borderBottom: '1px solid white' }}>
-          <h3 className={style.h3}>
-            <FontAwesomeIcon icon={faAddressCard} />
-            { ' About' }
-          </h3>
-        </NavLink>
+    <nav>
+      <NavLink to="/" className={style.link}>
+        <img className={style.logo} src={logo} alt="logo" />
+      </NavLink>
+      <div className={style.mobileMenu}>
+        hola
+      </div>
+      <div className={style.menu}>
+        <NavLink to="/about" className={style.link} activeClassName={style.active}>About</NavLink>
+        <NavLink to="/signin" className={style.signin} activeClassName={style.active}>Sign In</NavLink>
       </div>
     </nav>
   );
