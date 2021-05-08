ed73ecf2acad6a8915bc615e7338b01e6812d022
avila sebastian <avila.sebastian1490@gmail.com>
2021-05-08 11:49:23 -0300

    fixes #11

--- api/src/controllers/posts.js
+++ api/src/controllers/posts.js
@@ -2,6 +2,7 @@
 const { v4: uuidv4 } = require('uuid');
 const { Op, Sequelize } = require('sequelize');
 const { Post, Property, Image } = require('../db.js');
+const { buidlWhere, getCurrentPage, getCurrentEndPoint } = require('../utils');
 
 async function addPost(req, res) {
   const id = uuidv4();
@@ -39,82 +40,52 @@ async function addPost(req, res) {
   await Property.create(newProperty);
   res.send({ message: 'Se creo exitosamente un nuevo post!' });
 }
+
+// Ejemplos de un pedido
 // http://localhost:3001/posts?city=med&neighborhood=pol&prop_type=Apartamento
 // http://localhost:3001/posts?city=med&neighborhood=pol&priceMin=0&priceMax=100000000
 async function getPosts(req, res) {
   const limit = 10;
   const offset = Number(req.query.offset) || 0;
-  const city = req.query.city || '';
-  const neighborhood = req.query.neighborhood || '';
-  // const prop_type = req.query.prop_type || '';
-  // const priceMin = Number(req.query.priceMin) || 0;
-  // const priceMax = Number(req.query.priceMax) || null;
-  //     prop_type: '', // select
-  // city: '', // input
-  // neighborhood: '', // input
+  const block = {
+    city: req.query.city || '',
+    neighborhood: req.query.neighborhood || '',
+    prop_type: req.query.prop_type || '',
+    priceMin: Number(req.query.priceMin) || 0,
+    priceMax: Number(req.query.priceMax) || null,
+    areaMin: Number(req.query.areaMin) || 0,
+    areaMax: Number(req.query.areaMax) || null,
+    stratum: Number(req.query.stratum) || null,
+    rooms: Number(req.query.rooms) || null,
+    bathrooms: Number(req.query.bathrooms) || null,
+    years: Number(req.query.years) || null,
+  };
 
-  // stratum: '', // select es un numero
-  // priceMin: '',
-  // priceMax: '',
-  // areaMin: '',
-  // areaMax: '',
-  // rooms: '', // input number
-  // bathrooms: '', // input number
-  // years: '', // input number
-
-  const { count, rows } = await Post.findAndCountAll(
-    {
-      limit,
-      offset,
-      attributes: ['id', 'post_name'],
+  const queryPost = {
+    limit,
+    offset,
+    attributes: ['id', 'post_name'],
+    include: {
+      model: Property,
+      attributes: ['id', 'prop_type', 'city', 'neighborhood', 'm2', 'price', 'stratum', 'rooms', 'bathrooms', 'years'],
+      where: buidlWhere(block),
       include: {
-        model: Property,
-        attributes: ['id', 'prop_type', 'city', 'neighborhood', 'rooms', 'm2', 'price'],
-        where: {
-          [Op.and]: [
-            {
-              city: {
-                [Op.iLike]: `%${city}%`,
-              },
-            },
-            {
-              neighborhood: {
-                [Op.iLike]: `%${neighborhood}%`,
-              },
-            },
-            // {https://sequelize.org/v5/manual/querying.html
-            //   prop_type: {
-            //     [Op.iLike]: `%${prop_type}%`
-            //     // [Sequelize.Op.in]: [prop_type],
-            //   }
-            // },
-            // {
-            //   price: {
-            //     [Op.and]: {
-            //       [Op.gte]: priceMin,
-            //       [Op.lte]: priceMax
-            //     }
-            //     // [Op.gte]:  priceMin
-            //     // [Sequelize.Op.between]: [priceMin, priceMax],
-            //   }
-            // }
-
-          ],
-        },
-        include: {
-          model: Image,
-          attributes: ['id', 'photo'],
-        },
+        model: Image,
+        attributes: ['id', 'photo'],
       },
     },
-  );
+  };
+
+  const { count, rows } = await Post.findAndCountAll(queryPost);
+
   return res.status(200).json(
     {
       count,
       posts: rows,
+      currentPage: getCurrentPage(offset, limit),
+      selfEndpoint: getCurrentEndPoint(block, offset),
     },
   );
-  // res.send({ message: 'Estos son todo las publicaciones' });
 }
 
 async function deletePost(req, res) {
---
+++ api/src/utils.js
@@ -0,0 +1,97 @@
+const { Op, Sequelize } = require('sequelize');
+// FUNCIONES AUXILIARES DE SEQUELIZE
+
+/**
+ * Busca la coincidencias dentro de un rango ve valores
+ * @param {* min es de tipo integer y es el minimo valor de busqueda} min
+ * @param {* max es de tipo integer y es el maximo valor de busqueda} max
+ * @returns Un objeto de busqueda: [Op.gte]: (>=) o [Op.lte]: (<=)
+ */
+function buildMinMax(min, max) {
+  // eslint-disable-next-line no-shadow
+  const query = [];
+  // { [Op.and]: [{},{},{}] }
+  if (min) query.push({ [Sequelize.Op.gte]: min });
+  if (max) query.push({ [Sequelize.Op.lte]: max });
+  return { [Sequelize.Op.and]: query };
+}
+
+/**
+   * Busca una las coincidencias (case insensitive) de una palabra (word) en un string
+   * @param {* word es de tipo string} word
+   * @returns Un objeto de busqueda: [Op.iLike]:
+   */
+function buildIlike(word) {
+  return { [Op.iLike]: `%${word}%` };
+}
+
+/**
+   * Busca el valor exacto
+   * @param {* number es de tipo integer } number
+   * @returns Un objeto de busqueda: [Op.eq]:
+   */
+function buildEqual(number) {
+  return { [Op.eq]: number };
+}
+
+/**
+   * Construye una sentencia where en conjunto con varios and
+   * @param {* block es un objeto con los atributos que se van a usar para los filtro } block
+   * @returns Las condiciones de busqueda: where atributo1 and atributo2 and ...
+   */
+function buidlWhere(block) {
+  // eslint-disable-next-line no-shadow
+  const query = [];
+  if (block.city) query.push({ city: buildIlike(block.city) });
+  if (block.neighborhood) query.push({ neighborhood: buildIlike(block.neighborhood) });
+  query.push({ price: buildMinMax(block.priceMin, block.priceMax) });
+  query.push({ m2: buildMinMax(block.areaMin, block.areaMax) });
+  if (block.stratum) query.push({ stratum: buildEqual(block.stratum) });
+  if (block.rooms) query.push({ rooms: buildEqual(block.rooms) });
+  if (block.bathrooms) query.push({ bathrooms: buildEqual(block.bathrooms) });
+  if (block.years) query.push({ years: buildEqual(block.years) });
+  if (block.prop_type) query.push({ prop_type: buildIlike(block.prop_type) });
+  return { [Sequelize.Op.and]: query };
+}
+
+// FUNCIONES AUXILIARES PARA EL PAGINADO
+/**
+ * Obtiene el numero de la pagina actual con respecto al offset y al limit
+ * @param {* offset es de tipo integer y es desde donde empiezo a traer los datos} offset
+ * @param {* limit es de tipo integer y es la cantidad de registros que devuelvo} limit
+ * @returns Un integer
+ */
+function getCurrentPage(offset, limit) {
+  return (offset + limit) / limit;
+}
+
+/**
+ * Reconstruye el Enpoint utilizado desde el front
+ * @param {* block es un objeto con los atributos que se usaron para los filtro} block
+ * @param {* offset es de tipo integer y es desde donde empiezo a traer los datos} offset
+ * @returns Un string (endpoint) con todas las re.query que se usaron
+ */
+function getCurrentEndPoint(block, offset) {
+  let endpoint = `http://localhost:3001/posts?offset=${offset}`;
+  if (block.city) endpoint += `&city=${block.city}`;
+  if (block.neighborhood) endpoint += `&neighborhood=${block.neighborhood}`;
+  if (block.priceMin) endpoint += `&priceMin=${block.priceMin}`;
+  if (block.priceMax) endpoint += `&priceMax=${block.priceMax}`;
+  if (block.areaMin) endpoint += `&areaMin=${block.areaMin}`;
+  if (block.areaMax) endpoint += `&areaMax=${block.areaMax}`;
+  if (block.stratum) endpoint += `&stratum=${block.stratum}`;
+  if (block.rooms) endpoint += `&rooms=${block.rooms}`;
+  if (block.bathrooms) endpoint += `&bathrooms=${block.bathrooms}`;
+  if (block.years) endpoint += `&years=${block.years}`;
+  if (block.prop_type) endpoint += `&prop_type=${block.prop_type}`;
+  return endpoint;
+}
+
+module.exports = {
+  buidlWhere,
+  buildEqual,
+  buildIlike,
+  buildMinMax,
+  getCurrentPage,
+  getCurrentEndPoint,
+};
