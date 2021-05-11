d64a39663a916ddf377a74f9256dee05521bdbc0
pablo-chaves <pablo161096@gmail.com>
2021-05-10 22:13:26 -0500

    add folder admin

---
+++ client/src/Components/PanelAdmin/PanelAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PanelAdmin.module.css';
+// import MyHouseRoutes from '../routes';
+
+function PanelAdmin() {
+  // return (
+  //   <div>
+  //   </div>
+  // );
+}
+
+export default PanelAdmin;
