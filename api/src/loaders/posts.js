71074fe1c1a896989ee56cdcfd5062cc14be3d51
mgimbatti <marcos.gimbatti@gmail.com>
2021-05-12 16:37:05 -0300

    Adds coordinates to DB

--- api/src/loaders/posts.js
+++ api/src/loaders/posts.js
@@ -1,15 +1,18 @@
+/* eslint-disable max-len */
 const { v4: uuidv4 } = require('uuid');
 const rawData = require('./rawData');
 const users = require('./users');
 const addresses = require('./addresses');
+const coordinates = require('./coordinates');
 
+const department = ['Cundinamarca', 'Antioquia', 'Bolívar'];
 const userIdList = users.map((u) => u.id);
 const premiumRate = 0.2;
 
 const posts = rawData.map(
   ({
     mtipoinmueble,
-    /* mciudad, */
+    mciudad,
     title,
     mzona,
     mbarrio,
@@ -18,15 +21,18 @@ const posts = rawData.map(
     mnrocuartos,
     mnrobanos,
     mnrogarajes,
-  }) => ({
+  }, index) => ({
     id: uuidv4(),
     userId: userIdList[Math.floor(Math.random() * userIdList.length + 1)],
     post_name: title,
     premium: Math.random() < premiumRate,
     prop_type: mtipoinmueble.nombre,
-    department: 'Antioquia' /* 'To be determined' */,
-    city: 'Medellín' /* mciudad.nombre */,
-    street_number: 'CC 15B N 25D 2S' /* addresses.shift()'To be determined' */,
+    // eslint-disable-next-line no-nested-ternary
+    department: mciudad.id === '1' ? department[0] : (mciudad.id === '2' ? department[1] : department[2]),
+    city: mciudad.nombre,
+    street_number: addresses[index],
+    latitude: coordinates[index].lat,
+    longitude: coordinates[index].lng,
     // zip_code: 'To be determined',
     description: title,
     stratum: mzona && mzona.id,
