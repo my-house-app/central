ac59a53b9507f04b003c1e1140f6e671d0a50464
mgimbatti <marcos.gimbatti@gmail.com>
2021-05-20 21:05:59 -0300

    Adds authentication to redux state

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 import logo from '../../images/logoHorizontal-6pt.svg';
 import AuthNav from '../Auth0/Auth-nav/Auth-nav';
 import style from './NavBar.module.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { getGoogleUserData, userSession } from '../../Redux/Actions/index';
 
-function NavBar() {
+function NavBar({ userInfo, getGoogleUser, userSession }) {
+  const { user, isAuthenticated } = useAuth0();
+  console.log(user)
+  console.log(isAuthenticated)
+  useEffect(() => {
+    if (isAuthenticated && !Object.keys(userInfo).length) {
+        if (user.sub.substring(0,6) === 'google'){
+          const googleUser = {
+            name: user.name,
+            email: user.email,
+            externalId: user.sub
+          }
+          getGoogleUser(googleUser);
+        } else {
+          const userId = user.sub.slice(6);
+          userSession(userId) 
+        }
+      }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  
   return (
     <nav>
       <NavLink to="/" className={style.link}>
@@ -19,4 +42,13 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+const mapStateToProps = (state) => ({
+  userInfo: state.session,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  userSession: (id) => dispatch(userSession(id)),
+  getGoogleUser: (googleUser) => dispatch(getGoogleUserData(googleUser)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
